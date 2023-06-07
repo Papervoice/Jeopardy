16dd7e6fa46a953857c415f8390ae53ff169f338
Andrew Chae <aschae@outlook.com>
2023-06-07 10:10:26 -0400

    Renamed examplefile.html to game7792.html due to the other html files that are copied and inserted into the project folder for reference. Also created seasonlisturl.html for reference and dissection later.

--- JeopardyProject/GameIdScraper.py
+++ JeopardyProject/GameIdScraper.py
@@ -1,3 +1,15 @@
 #import requests/beautifulSoup library
+"""
+Using beautifulSoup4 and requests, GameIdScraper.py will be able to traverse https://j-archive.com/listseasons.php
+and retrieve the urls of each season. When it retrieves each url for each sesason, it will be able to 
+retrieve the game id, and therefore the url, for each episode. It will then be able to send the game ids
+to EpisodeScraping.py
+"""
 import requests
 from bs4 import BeautifulSoup
+
+url = "https://j-archive.com/listseasons.php"
+response = requests.get(url)
+soup = BeautifulSoup(response.content, "html.parser")
+
+print(soup.prettify())
